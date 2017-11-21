e17488bbf609baee1b9ec42d2fc88f3afb8ed8cf
Jim Posen <jimpo@coinbase.com>
2017-11-20 17:47:01 -0800

    Package documentation for btcexport.

---
+++ doc.go
@@ -0,0 +1,9 @@
+/**
+* package btcexport provides a service for parsed the blockchain as stored by a
+* btcd full node and exporting it as CSV in chunked files. The BlockExporter
+* struct runs a concurrent process to perform the export and BlockEncoder
+* defines the transformation from deserialized btcd structures to CSV records.
+* The BlockExporter supports multiple file output backends, currently including
+* filesystem output and uploads to Amazon S3 buckets.
+ */
+package btcexport
